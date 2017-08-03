08ada62bb67fabfa599a2e1740719707d0c1e935
darrenfu <tiger_kin@163.com>
2017-08-03 18:01:01 +0800

    update httpclient pool

--- resty-pass/src/main/java/df/open/restypass/http/config/AsyncHttpConfigFactory.java
+++ resty-pass/src/main/java/df/open/restypass/http/config/AsyncHttpConfigFactory.java
@@ -1,7 +1,9 @@
 package df.open.restypass.http.config;
 
+import io.netty.util.HashedWheelTimer;
 import org.asynchttpclient.AsyncHttpClientConfig;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
+import org.asynchttpclient.netty.channel.DefaultChannelPool;
 
 /**
  * HttpClient config 工厂类
@@ -16,12 +18,21 @@ public class AsyncHttpConfigFactory {
      * @return the config
      */
     public static AsyncHttpClientConfig createConfig(int connectTimeout, int requestTimeout) {
+        HashedWheelTimer timer = new HashedWheelTimer();
+        timer.start();
+        DefaultChannelPool channelPool = new DefaultChannelPool(60000,
+                -1,
+                DefaultChannelPool.PoolLeaseStrategy.LIFO,
+                timer,
+                1000);
+
         return new DefaultAsyncHttpClientConfig.Builder()
                 .setConnectTimeout(connectTimeout)
                 .setRequestTimeout(requestTimeout)
                 .setMaxConnectionsPerHost(10000)
                 .setValidateResponseHeaders(false)
                 .setMaxRequestRetry(0)
+                .setChannelPool(channelPool)
                 .build();
     }
 
